9784be2fb31f28bff1020e448b3bc42c3815ff2c
ZeroHX <61070023@kmitl.ac.th>
2018-12-31 00:57:28 +0700

    Add Lab

---
+++ lab/Sec1/a0/a0.c
@@ -0,0 +1,13 @@
+/*
+==================================
+Born2DEV : C-programming zero2one
+[Section 1] Assignment 0 : ร้านของลุงสมหมาย
+==================================
+inp: -
+out: Welcome to Sommai 108 Eleven Shop
+
+*/
+#include <stdio.h>
+int main(){
+    printf("Welcome to Sommai 108 Eleven Shop");
+}
---
+++ lab/Sec1/a1/a1.c
@@ -0,0 +1,24 @@
+/*
+==================================
+Born2DEV : C-programming zero2one
+[Section 1] Assignment 1 : จุดกำเนิดเด็กหญิงสมจริง
+==================================
+Problem: Print pattern below
+inp: -
+out:    *
+        **
+        ***
+        ****
+        *****
+        ******
+
+*/
+#include <stdio.h>
+int main(){
+    printf("*\n");
+    printf("**\n");
+    printf("***\n");
+    printf("****\n");
+    printf("*****\n");
+    printf("******");
+}
