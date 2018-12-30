e0695932e41b4240c98c05f15654e8a7cae830b3
ZeroHX <61070023@kmitl.ac.th>
2018-12-30 22:24:42 +0700

    add lec23

---
+++ sec5/lec23/lec23.c
@@ -0,0 +1,14 @@
+/*
+==================================
+Born2DEV : C-programming zero2one
+Section3, Lecture23 [Function Random]
+==================================
+*/
+#include <stdio.h>
+#include <stdlib.h>
+int main(void){
+    int a = rand();
+
+    printf("The lucky number is %d",&a);
+    return 0;
+}
